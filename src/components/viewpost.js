642ce7112deb88e61563c04dabe98d5db5b2a403
Tom Flatters <tflats.tf@gmail.com>
2020-09-07 22:05:41 +0100

    Redirect bad URLs to homepage'

--- src/components/viewpost.js
+++ src/components/viewpost.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import '../App.css';
 import * as firebase from 'firebase';
+import { Redirect } from 'react-router';
 
 class ViewPost extends Component {
   
   constructor(){
     super();
     this.state = {
-      data: {
         'title': '',
         'authorUsername': '',
         'charity': '',
@@ -18,7 +18,8 @@ class ViewPost extends Component {
         'subtitle': '',
         'targetAmount': '',
         'timestamp': '',
-      }
+        'loading': true,
+        'postNotFound': false,
     };
   }
 
@@ -37,11 +38,14 @@ class ViewPost extends Component {
               'noLikes': d.noLikes,
               'subtitle': d.subtitle,
               'targetAmount': d.targetAmount,
-              'timestamp': d.timestamp
+              'timestamp': d.timestamp,
+              'loading': false
             })
         } else {
             // TODO: redirect to home
-            
+            this.setState({
+                'postNotFound': true
+            })
         }
         
     });
@@ -59,51 +63,62 @@ class ViewPost extends Component {
   }
 
   render(){
-    return (
-      <div className="App">
-        {/* container */}
-        <div className="post-container">
-            <div className="post-container-inner">
-            {/* row for author name, pic and charity logo */}
-            <div className="row space-apart mb">
-                <div className="row">
-                    <div>pic</div>
-                    <div className="pl">{this.state.authorUsername}</div>
-                </div>
-                <div>{this.state.charity}</div>
-            </div>
-            {/* video/image */}
-            <div className="row center">
-                { this.isVideo() ? <video controls className="post-image" src={this.state.imageUrl}/> : <img className="post-image" src={this.state.imageUrl}></img>}
-            </div>
-            {/* text container: title, descr., money raised all in this */}
-            <div className="mb">
-                <h2>{this.state.title}</h2>
-                <p>{this.state.subtitle}</p>
-                <div>
-                    <div class="progress-container">
-                        <div class="progress-bar" style={{'width': (100*this.state.moneyRaised/this.state.targetAmount).toString()+'%'}}></div>
+    if(this.state.postNotFound){
+        return(<Redirect to='/'/>)
+    }
+    else{
+        if(this.state.loading){
+            return(
+                <div></div>
+            )
+        } else{
+        return (
+        <div className="App">
+            {/* container */}
+            <div className="post-container">
+                <div className="post-container-inner">
+                {/* row for author name, pic and charity logo */}
+                <div className="row space-apart mb">
+                    <div className="row">
+                        <div>pic</div>
+                        <div className="pl">{this.state.authorUsername}</div>
                     </div>
-                    £{this.state.moneyRaised} raised of £{this.state.targetAmount} target.
+                    <div>{this.state.charity}</div>
+                </div>
+                {/* video/image */}
+                <div className="row center">
+                    { this.isVideo() ? <video controls className="post-image" src={this.state.imageUrl}/> : <img className="post-image" src={this.state.imageUrl}></img>}
+                </div>
+                {/* text container: title, descr., money raised all in this */}
+                <div className="mb">
+                    <h2>{this.state.title}</h2>
+                    <p>{this.state.subtitle}</p>
+                    <div>
+                        <div class="progress-container">
+                            <div class="progress-bar" style={{'width': (100*this.state.moneyRaised/this.state.targetAmount).toString()+'%'}}></div>
+                        </div>
+                        £{this.state.moneyRaised} raised of £{this.state.targetAmount} target.
+                    </div>
+                </div>
+                {/* Icon panel: likes, comments, share button */}
+                <div className="row space-apart mb">
+                    <div>{this.state.noLikes} Likes</div>
+                    <div>Share</div>
+                </div>
+                {/* Time since post */}
+                <div className="mb">17 hour(s) ago</div>
+                {/* Donate button */}
+                <div className="row center">
+                    <button className="primary-button">
+                        Donate
+                    </button>
                 </div>
             </div>
-            {/* Icon panel: likes, comments, share button */}
-            <div className="row space-apart mb">
-                <div>{this.state.noLikes} Likes</div>
-                <div>Share</div>
-            </div>
-            {/* Time since post */}
-            <div className="mb">17 hour(s) ago</div>
-            {/* Donate button */}
-            <div className="row center">
-                <button className="primary-button">
-                    Donate
-                </button>
             </div>
         </div>
-        </div>
-      </div>
-    );
+        );
+        }
+    }
   }
 }
 
