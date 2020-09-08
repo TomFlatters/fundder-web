f8722c4c0749d79076ef619df253c691ad654ee2
Tom Flatters <tflats.tf@gmail.com>
2020-09-08 12:32:02 +0100

    Finish basic styling

--- src/components/viewpost.js
+++ src/components/viewpost.js
@@ -3,6 +3,9 @@ import '../App.css';
 import * as firebase from 'firebase';
 import { Redirect } from 'react-router';
 
+import like from '../img/like.png';
+import share from '../img/share.png';
+
 class ViewPost extends Component {
   
   constructor(){
@@ -20,42 +23,59 @@ class ViewPost extends Component {
         'timestamp': '',
         'loading': true,
         'postNotFound': false,
+        'profileImageUrl': '',
+        'userId': '',
+        'charityImageUrl': '',
     };
   }
 
   componentDidMount(){
     const postId = this.props.match.params.id;
+    var d;
     firebase.firestore().collection('posts').doc(postId).get().then((post) => {
         if (post.exists) {
-            var d = post.data();
+            d = post.data();
+            console.log('got post data');
             this.setState({
-              'title': d.title,
-              'authorUsername': d.authorUsername,
-              'charity': d.charity,
-              'hashtags': d.hashtags,
-              'imageUrl': d.imageUrl,
-              'moneyRaised': d.moneyRaised,
-              'noLikes': d.noLikes,
-              'subtitle': d.subtitle,
-              'targetAmount': d.targetAmount,
-              'timestamp': d.timestamp,
-              'loading': false
-            })
+                'title': d.title,
+                'authorUsername': d.authorUsername,
+                'userId': d.author,
+                'charity': d.charity,
+                'hashtags': d.hashtags,
+                'imageUrl': d.imageUrl,
+                'moneyRaised': d.moneyRaised,
+                'noLikes': d.noLikes,
+                'subtitle': d.subtitle,
+                'targetAmount': d.targetAmount,
+                'timestamp': d.timestamp,
+                'loading': false,
+                'charityLogo': d.charityLogo,
+                });
+            // this.getImage(d.author);
         } else {
             // TODO: redirect to home
             this.setState({
                 'postNotFound': true
             })
         }
-        
     });
+
   }
+
+//   getImage = (id) => {
+//     console.log('getting pic');
+//     console.log(id);
+//     firebase.firestore().collection('users').doc(id).get().then((userProfile) => {
+//         console.log('got user pic');
+//         this.setState({
+//             'profileImageUrl': userProfile.profilePic
+//         })
+//     });
+//   }
 
   isVideo = () => {
     let url = this.state.imageUrl;
-    console.log(url);
     if (url && url!==''){
-        console.log(url.indexOf('/o/videos'));
         return url.indexOf('/o/videos')===-1 ? false : true;
     } else {
         return false
@@ -63,6 +83,7 @@ class ViewPost extends Component {
   }
 
   render(){
+    console.log(this.state);
     if(this.state.postNotFound){
         return(<Redirect to='/'/>)
     }
@@ -78,37 +99,39 @@ class ViewPost extends Component {
             <div className="post-container">
                 <div className="post-container-inner">
                 {/* row for author name, pic and charity logo */}
-                <div className="row space-apart mb">
-                    <div className="row">
-                        <div>pic</div>
-                        <div className="pl">{this.state.authorUsername}</div>
-                    </div>
-                    <div>{this.state.charity}</div>
+                <div className="px row space-apart center-vertically mb">
+                    <div className="w-500">{this.state.authorUsername}</div>
+                    <div className="charity-logo-container"><img className="charity-logo" src={this.state.charityLogo}/></div>
                 </div>
                 {/* video/image */}
                 <div className="row center">
                     { this.isVideo() ? <video controls className="post-image" src={this.state.imageUrl}/> : <img className="post-image" src={this.state.imageUrl}></img>}
                 </div>
                 {/* text container: title, descr., money raised all in this */}
-                <div className="mb">
-                    <h2>{this.state.title}</h2>
-                    <p>{this.state.subtitle}</p>
+                <div className="mb px">
                     <div>
-                        <div class="progress-container">
-                            <div class="progress-bar" style={{'width': (100*this.state.moneyRaised/this.state.targetAmount).toString()+'%'}}></div>
+                        <h3>{this.state.title}</h3>
+                        <p>
+                            {this.state.subtitle} 
+                            {this.state.hashtags.map((hashtag) => <span className="hashtag"> #{hashtag}</span>)}
+                        </p>
+                    </div>
+                    <div className="w-500 mt">
+                        <div className="progress-container">
+                            <div className="progress-bar" style={{'width': (100*this.state.moneyRaised/this.state.targetAmount).toString()+'%'}}></div>
                         </div>
-                        £{this.state.moneyRaised} raised of £{this.state.targetAmount} target.
+                        £{this.state.moneyRaised>0 ? this.state.moneyRaised : '0.00'} raised of £{this.state.targetAmount} target.
                     </div>
                 </div>
                 {/* Icon panel: likes, comments, share button */}
-                <div className="row space-apart mb">
-                    <div>{this.state.noLikes} Likes</div>
-                    <div>Share</div>
+                <div className="row space-apart py mt px2">
+                    <div className="w-500 row"><img className="like-symbol"src={like}/><div className="pl">{this.state.noLikes}</div></div>
+                    <div className="w-500 row"><img className="like-symbol"src={share}/><div className="pl">Share</div></div>
                 </div>
                 {/* Time since post */}
-                <div className="mb">17 hour(s) ago</div>
+                <div className="w-300 mb2 px2">17 hour(s) ago</div>
                 {/* Donate button */}
-                <div className="row center">
+                <div className="row center px">
                     <button className="primary-button">
                         Donate
                     </button>
